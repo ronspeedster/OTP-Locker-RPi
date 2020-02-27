ca8f4be9e1d214f41ce93e455aedd02fc32fe11a
Ronie Bituin <ronieB03@gmail.com>
2020-02-27 16:51:01 +0800

    2020-02-27

--- locker/index.php
+++ locker/index.php
@@ -65,7 +65,7 @@
 					<div class="flex-sb-m w-full p-b-48" style="display: none;">
 						<div class="contact100-form-checkbox">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
+                            <label class="label-checkbox100" for="ckb1">
 								Remember me
 							</label>
 						</div>
